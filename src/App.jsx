7edcf1fcde353a2d4c198ada1d228fe3af22371f
coghoyi <davidhoyolopez@alumnostrazos.net>
2023-06-07 14:22:06 +0200

    React-State

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
+
 const App = () => {
-	return <h1>Núcleo de la aplicación</h1>;
+	const [counter, setCounter] =useState(0);
+
+	return (
+		<>
+		<h1>{counter}</h1>
+		<button onClick={()=>handleClick(counter, setCounter)}>CLICK ME</button>
+		</>
+	)
 };
+
+
+const handleClick = (counter, setCounter) =>{
+		setCounter(counter + 1);
+	}
+
+
 
 export default App;
